53aaa7a350e2b7e61aa81aeb0217c8f4c25cfc07
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2014-09-18 13:47:47 +0000

    add cert

---
+++ manifests/cert.pp
@@ -0,0 +1,41 @@
+# = Define: apache::cert
+#
+# This define deploys certificates.
+#
+define apache::cert (
+  $domain,
+  $type,
+  $c_owner = 'root',
+  $c_group = 'root',
+  $c_mode = 0400,
+  $c_notify = [],
+  $directory = '/etc/pki/tls',
+  $source = '/modules/apache/tls',
+) {
+
+  # FIXME: we could guess domain and type from name?
+  case $type {
+    'crt': {
+      $dir = 'certs'
+      $filename = "${domain}.${type}"
+    }
+    'key': {
+      $dir = 'private'
+      $filename = "${domain}.${type}"
+    }
+    default: {
+      fail("Unhandled apache::cert type ${type}")
+    }
+
+  }
+
+  file {"${directory}/${dir}/${filename}":
+    ensure => file,
+    source => "puppet://${source}/${dir}/${filename}",
+    owner  => $c_owner,
+    group  => $c_group,
+    mode   => $c_mode,
+    notify => $c_notify
+  }
+  
+}
