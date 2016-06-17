8796b9b9a8ec71c88595ebf07a31910f9f712ac9
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2016-06-17 18:10:01 -0400

    add $includes to apache::vhost::http, similar to https

--- manifests/vhost/http.pp
+++ manifests/vhost/http.pp
@@ -11,6 +11,11 @@ define apache::vhost::http (
   $apache_template='apache/vhost/http/vhost-http.inc.erb',
   $server_name='localhost.localdomain',
   $server_aliases=undef,
+  $includes=undef,
+  # FIXME: this parameter is messy compared to https
+  # invoking with default_include=undef will set it to the value below
+  # we want to make it possible to not have any default_include
+  # and not have any DocumentRoot
   $default_include="container-http-${title}.inc"
 ) {
 
